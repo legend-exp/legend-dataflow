7c77c7404b9025f79c17abb92863cdb713015e9d
George Marshall <marshall@legend-login2.lngs.infn.it>
2022-07-04 11:39:24 +0200

    Bugfixes

--- scripts/build_raw.py
+++ scripts/build_raw.py
@@ -1,4 +1,5 @@
 import argparse, os, pathlib
+import logging
 
 import pygama
 from pygama.raw.build_raw import * 
@@ -7,19 +8,24 @@ argparser = argparse.ArgumentParser()
 argparser.add_argument("input", help="input file", type=str)
 argparser.add_argument("output", help="output file", type=str)
 argparser.add_argument("--configs", help="config file", type=str)
+argparser.add_argument("--log", help="log file", type=str)
 args = argparser.parse_args()
+
+logging.basicConfig(level=logging.INFO, filename=args.log, filemode='w')
 
 pathlib.Path(os.path.dirname(args.output)).mkdir(parents=True, exist_ok=True)
 
 # ToDo: Atomic file creation
 
-out_spec = {
+out_spec = '''
+{
   "ORFlashCamADCWaveformDecoder" : {
     "ch{key:03d}/raw" : {
       "key_list" : ["*"],
-      "out_stream" : args.output
+      "out_stream" : "filekey"
     }
   }
 }
+'''
 
-build_raw(args.input, in_stream_type='ORCA', out_spec=out_spec)
+build_raw(args.input, in_stream_type='ORCA', out_spec=json.loads(out_spec_json), filekey = args.output)
