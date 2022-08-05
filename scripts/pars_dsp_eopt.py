3e715df0326c67742cf4fcfff8a5128321a7f32c
George Marshall <marshall@legend-login2.lngs.infn.it>
2022-08-05 11:17:10 +0200

    Changes to use new optimisation

--- scripts/pars_dsp_eopt.py
+++ scripts/pars_dsp_eopt.py
@@ -1,12 +1,239 @@
 import json, os
-import pygama.pargen.energy_optimisation as om
-import pygama.math.peak_fitting as pgf
 from collections import OrderedDict
 import argparse
 import pathlib
 import pickle as pkl
 import numpy as np
 from util.metadata_loading import *
+
+import logging
+import time
+
+from pygama.pargen.dsp_optimize import run_one_dsp
+import pygama.pargen.energy_optimisation as om
+import pygama.lgdo.lh5_store as lh5
+import pygama.math.peak_fitting as pgf
+
+def get_wf_indexes(sorted_indexs, n_events):
+    out_list = []
+    if isinstance(n_events, list):
+        for i in range(len(n_events)):
+            new_list = []
+            for idx,entry in enumerate(sorted_indexs):
+                if (entry >= np.sum(n_events[:i])) and (entry < np.sum(n_events[:i+1])):
+                    new_list.append(idx)
+            out_list.append(new_list)       
+    else:
+        for i in range(int(len(sorted_indexs)/n_events)):
+            new_list = []
+            for idx,entry in enumerate(sorted_indexs):
+                if (entry >= i*n_events) and (entry < (i+1)*n_events):
+                    new_list.append(idx)
+            out_list.append(new_list)
+    return out_list  
+
+argparser = argparse.ArgumentParser()
+#argparser.add_argument("files", help="files", nargs='*',type=str)
+argparser.add_argument("--raw_filelist", help="raw_filelist", type=str)
+argparser.add_argument("--decay_const", help="decay_const", type=str, required=True)
+argparser.add_argument("--configs", help="configs", type=str, required=True)
+
+argparser.add_argument("--log", help="log_file", type=str)
+
+argparser.add_argument("--datatype", help="Datatype", type=str, required=True)
+argparser.add_argument("--timestamp", help="Timestamp", type=str, required=True)
+argparser.add_argument("--channel", help="Channel", type=str, required=True)
+
+argparser.add_argument("--final_dsp_pars", help="final_dsp_pars", type=str, required=True)
+argparser.add_argument("--alpha_dict", help="alpha_dict", type=str, required=True)
+argparser.add_argument("--qbb_grid_path", help="qbb_grid_path", type=str)
+
+argparser.add_argument("--plot_save_path", help="plot_save_path", type=str, required=False)
+args = argparser.parse_args()
+    
+logging.basicConfig(level=logging.DEBUG, filename=args.log, filemode='w')
+logging.getLogger('numba').setLevel(logging.INFO)
+logging.getLogger('parse').setLevel(logging.INFO)
+logging.getLogger('pygama.lgdo.lh5_store').setLevel(logging.INFO)
+logging.getLogger('h5py._conv').setLevel(logging.INFO)
+logging.getLogger('pygama.dsp.processing_chain').setLevel(logging.INFO)
+
+
+log = logging.getLogger(__name__)
+
+
+t0 = time.time()
+
+cfg_file = os.path.join(args.configs, 'key_resolve.jsonl')
+configs = config_catalog.get_config(cfg_file, args.configs, args.timestamp, args.datatype)
+dsp_config = configs['snakemake_rules']['pars_dsp_eopt']["inputs"]['processing_chain'][args.channel]
+opt_json = configs['snakemake_rules']['pars_dsp_eopt']["inputs"]["optimiser_config"][args.channel]
+
+with open(opt_json, 'r') as r:
+    opt_dict = json.load(r)
+
+with open(args.raw_filelist) as f:
+    files = f.read().splitlines()
+
+raw_files = sorted(files)
+
+with open(args.decay_const, 'r') as t:
+    db_dict = json.load(t)
+
+
+peaks_keV = np.array([583.191, 727.330, 860.564, 1620.5, 2614.553])
+kev_widths = [(25,30), (25,35),(25,40),(25,55), (70,70)]
+funcs = [pgf.gauss_step_pdf, pgf.radford_pdf, pgf.radford_pdf,pgf.radford_pdf,pgf.radford_pdf, pgf.radford_pdf]
+
+final_idxs = []
+kwarg_dicts_cusp = []
+kwarg_dicts_trap = []
+kwarg_dicts_zac = []
+n_events=[]
+for peak in peaks_keV:
+    peak_idx = np.where(peaks_keV == peak)[0][0]
+    func = funcs[peak_idx]
+    kev_width = kev_widths[peak_idx]
+    idxs = om.event_selection(raw_files, f'{args.channel}/raw', dsp_config, db_dict, peaks_keV, peak_idx, kev_width)
+    final_idxs += idxs.tolist()
+    n_events.append(len(idxs))
+    kwarg_dicts_cusp.append({'parameter':'cuspEmax', 'func':pgf.extended_radford_pdf, 
+                'gof_func':pgf.radford_pdf,'peak':peak, 'kev_width':kev_width})
+    kwarg_dicts_zac.append({'parameter':'zacEmax', 'func':pgf.extended_radford_pdf, 
+                'gof_func':pgf.radford_pdf,'peak':peak, 'kev_width':kev_width})
+    kwarg_dicts_trap.append({'parameter':'trapEmax', 'func':pgf.extended_radford_pdf, 
+                'gof_func':pgf.radford_pdf,'peak':peak, 'kev_width':kev_width})
+
+sort_index = np.argsort(final_idxs)
+idx_list = get_wf_indexes(sort_index, n_events)
+idxs = np.array(sorted(final_idxs))
+sto =lh5.LH5Store()
+
+baseline = sto.read_object(f'{args.channel}/raw/baseline', raw_files,idx=idxs, n_rows = np.sum(n_events))[0]
+waveforms = sto.read_object(f'{args.channel}/raw/waveform', raw_files,idx=idxs, n_rows = np.sum(n_events))[0]
+tb_data = lh5.Table(col_dict = { 'waveform' : waveforms, 'baseline':baseline } )
+
+t1 = time.time()
+log.info(f'Data Loaded in {(t1-t0)/60} minutes')
+
+
+kwarg_dict = [{'peak_dicts':kwarg_dicts_cusp, 'ctc_param':'QDrift', 'idx_list':idx_list, 'peaks_keV': peaks_keV},
+              {'peak_dicts':kwarg_dicts_zac, 'ctc_param':'QDrift', 'idx_list':idx_list, 'peaks_keV': peaks_keV},
+              {'peak_dicts':kwarg_dicts_trap, 'ctc_param':'QDrift', 'idx_list':idx_list, 'peaks_keV': peaks_keV}]
+
+
+sample_x = np.array([[1,1],[1,5], [12,1], [12,5], [4,3], [8,3]])
+sample_y = []
+sample_y_cusp = []
+results_cusp = []
+sample_y_zac = []
+results_zac = []
+sample_y_trap = []
+results_trap = []
+for i,x in enumerate(sample_x):
+    
+    db_dict["cusp"] = {"sigma":f'{x[0]}*us', "flat":f'{x[1]}*us'}
+    db_dict["zac"] = {"sigma":f'{x[0]}*us', "flat":f'{x[1]}*us'}
+    db_dict["etrap"] = {"rise":f'{x[0]}*us', "flat":f'{x[1]}*us'}
+
+    log.info(f'Initialising values {i+1} : {db_dict}')
+    
+    tb_out = run_one_dsp(tb_data,
+                dsp_config,
+                db_dict=db_dict,
+                verbosity=0)
+    
+    res = om.new_fom(tb_out, kwarg_dict[0])
+    if np.isnan(res['y_val']):
+        res['y_val']=15
+    sample_y_cusp.append(res['y_val'])
+    results_cusp.append(res)
+    res = om.new_fom(tb_out, kwarg_dict[1])
+    if np.isnan(res['y_val']):
+        res['y_val']=15
+    sample_y_zac.append(res['y_val'])
+    res = om.new_fom(tb_out, kwarg_dict[2])
+    results_zac.append(res)
+    if np.isnan(res['y_val']):
+        res['y_val']=15
+    sample_y_trap.append(res['y_val'])
+    results_trap.append(res)
+    log.info(f'{i+1} Finished')
+
+#n_events_cusp = [res["n_sig"] for res in results_cusp]
+#allowed_n_sig_cusp = 
+
+
+bopt_cusp = om.BayesianOptimizer(acq_func="lcb",batch_size=10)
+bopt_cusp.add_dimension("cusp", "sigma", 1, 16, "us")
+bopt_cusp.add_dimension("cusp", "flat", 1, 5, "us")
+
+bopt_zac = om.BayesianOptimizer(acq_func="lcb",batch_size=10)
+bopt_zac.add_dimension("zac", "sigma", 1, 16, "us")
+bopt_zac.add_dimension("zac", "flat", 1, 5, "us")
+
+bopt_trap = om.BayesianOptimizer(acq_func="lcb",batch_size=10)
+bopt_trap.add_dimension("etrap", "rise", 1, 12, "us")
+bopt_trap.add_dimension("etrap", "flat", 1, 5, "us")
+
+bopt_cusp.add_initial_values(x_init=sample_x, y_init=sample_y_cusp)
+bopt_zac.add_initial_values(x_init=sample_x, y_init=sample_y_zac)
+bopt_trap.add_initial_values(x_init=sample_x, y_init=sample_y_trap)
+
+best_idx = np.nanargmin(sample_y_cusp)
+bopt_cusp.optimal_results = results_cusp[best_idx]
+bopt_cusp.optimal_x = sample_x[best_idx]
+
+best_idx = np.nanargmin(sample_y_zac)
+bopt_zac.optimal_results = results_zac[best_idx]
+bopt_zac.optimal_x = sample_x[best_idx]
+
+best_idx = np.nanargmin(sample_y_trap)
+bopt_trap.optimal_results = results_trap[best_idx]
+bopt_trap.optimal_x = sample_x[best_idx]
+
+optimisers = [bopt_cusp,bopt_zac,bopt_trap]
+
+out_param_dict, out_results_list = om.run_optimisation(tb_data, dsp_config, [om.new_fom], optimisers, 
+                                                    fom_kwargs=kwarg_dict,db_dict=db_dict, n_iter=20)
+
+db_dict.update(out_param_dict)
+
+t2 = time.time()
+
+log.info(f'Optimiser finished in {(t2-t1)/60} minutes')
+
+pathlib.Path(os.path.dirname(args.final_dsp_pars)).mkdir(parents=True, exist_ok=True)
+with open(args.final_dsp_pars, 'w') as w:
+    json.dump(db_dict, w, indent=4)
+
+out_alpha_dict = {}
+out_alpha_dict["cusp_ctc"] = {"inputs":["cuspEmax", "dt_eff"],
+                              "outputs":["cuspEmax_ctc"],
+                              "function": "cuspEmax*(1+dt_eff*a)",
+                              "pars":{"a":bopt_cusp.optimal_results["alpha"]}}
+
+out_alpha_dict["zac_ctc"] = {"inputs":["zacEmax", "dt_eff"],
+                              "outputs":["zacEmax_ctc"],
+                              "function": "zacEmax*(1+dt_eff*a)",
+                              "pars":{"a":bopt_zac.optimal_results["alpha"]}}
+    
+out_alpha_dict["trap_ctc"] = {"inputs":["trapEmax", "dt_eff"],
+                              "outputs":["trapEmax_ctc"],
+                              "function": "trapEmax*(1+dt_eff*a)",
+                              "pars":{"a":bopt_trap.optimal_results["alpha"]}}
+
+
+pathlib.Path(os.path.dirname(args.alpha_dict)).mkdir(parents=True, exist_ok=True)
+with open(args.alpha_dict, 'w') as w:
+    json.dump(out_alpha_dict, w, indent=4)
+
+
+pathlib.Path(os.path.dirname(args.qbb_grid_path)).mkdir(parents=True, exist_ok=True)
+with open(args.qbb_grid_path,"wb") as f:
+    pkl.dump(optimisers, f)
+
+
 
 def match_config(parameters, opt_dicts):
     """
@@ -25,7 +252,7 @@ def match_config(parameters, opt_dicts):
 
 def load_all_grids(files, parameters):
     """
-    Loads in optimizer grids
+    #Loads in optimizer grids
     """
     grid_dict = {}
     for param in parameters:
@@ -36,6 +263,7 @@ def load_all_grids(files, parameters):
             peak_grids.append(grid[param])
         grid_dict[param] = peak_grids
     return grid_dict
+"""
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument("files", help="files", nargs='*',type=str)
@@ -137,3 +365,4 @@ with open(args.final_dsp_pars, 'w') as w:
 pathlib.Path(os.path.dirname(args.qbb_grid_path)).mkdir(parents=True, exist_ok=True)
 with open(args.qbb_grid_path,"wb") as f:
     pkl.dump({},f)
+"""
