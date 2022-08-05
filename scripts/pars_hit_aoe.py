98b345ff3a07d8abdf2a52f61058e99495946b7a
George Marshall <marshall@legend-login2.lngs.infn.it>
2022-08-05 11:18:06 +0200

    Added logs, configs, changes to work on l60 data

--- scripts/pars_hit_aoe.py
+++ scripts/pars_hit_aoe.py
@@ -2,124 +2,55 @@ import numpy as np
 import os,json
 import pathlib
 import argparse
+import logging
 
-from pygama.pargen.aoe_cal import cal_aoe
+from pygama.pargen.AoE_cal import cal_aoe
 
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument("files", help="files", nargs='*',type=str)
 argparser.add_argument("--ecal_file", help="ecal_file",type=str, required=True)
+argparser.add_argument("--eres_file", help="eres_file",type=str, required=True)
+
 argparser.add_argument("--configs", help="configs", type=str, required=True)
+argparser.add_argument("--datatype", help="Datatype", type=str, required=True)
+argparser.add_argument("--timestamp", help="Timestamp", type=str, required=True)
+argparser.add_argument("--channel", help="Channel", type=str, required=True)
+
+argparser.add_argument("--log", help="log_file", type=str)
+
 argparser.add_argument("--plot_file", help="plot_file",type=str)
 argparser.add_argument("--hit_pars", help="hit_pars",type=str)
+argparser.add_argument("--aoe_results", help="aoe_results",type=str)
 args = argparser.parse_args()
 
-with open(args.files[0]) as f:
-    files = f.read().splitlines()
-    files = sorted(files)
+logging.basicConfig(level=logging.DEBUG, filename=args.log, filemode='w')
+logging.getLogger('numba').setLevel(logging.INFO)
+logging.getLogger('parse').setLevel(logging.INFO)
+
+
+files = sorted(args.files)
 
 with open(args.ecal_file, 'r') as o:
     cal_dict = json.load(o)
 
+with open(args.eres_file, 'r') as o:
+    eres_dict = json.load(o)
+
 energy_param = 'cuspEmax'
 cal_energy_param = 'cuspEmax_ctc'
+
+eres_pars = [eres_dict[cal_energy_param]['m0'],eres_dict[cal_energy_param]['m1']]
     
 
-#out_dict = cal_aoe(files, cal_dict, energy_param, cal_energy_param, dt_corr=False, cut_parameters=cut_parameters, plot_savepath=args.plot_file)
+cal_dict, out_dict = cal_aoe(files, f'{args.channel}/dsp',cal_dict, energy_param, cal_energy_param, eres_pars,
+                            dt_corr=False, cut_parameters={}, plot_savepath=args.plot_file)
 
-"""
-cal_dict = "ecal": {
-            "function": "a+b*x",
-            "parameter": "cuspEmax_ctc",
-            "params":{
-                "a":1,
-                "b":1
-            }
-        },
-        "eres":{
-            "function": "sqrt(a+b*x)",
-            "parameter": "cuspEmax_ctc",
-            "params":{
-                "a":1,
-                "b":1
-            }
-        },
-        "aoecal": {
-            "current_parameter": "A_max",
-            "energy_parameter": "cuspEmax",
-            "cal_energy_parameter": "cuspEmax_ctc",
-            "mean":{
-                "function": "a+b*x",
-                "params":{
-                    "a":1,
-                    "b":1
-                    }
-                },
-            "sigma":{
-                "function": "sqrt(a+(B/x)**c)",
-                "params":{
-                    "a":1,
-                    "b":1,
-                    "c":1
-                }
-            }
-        }
-"""
 
-#out_dict = {"ecal_pars":cal_dict,"aoe_pars":cal_dict}
+pathlib.Path(os.path.dirname(args.hit_pars)).mkdir(parents=True, exist_ok=True)
+with open(args.hit_pars, 'w') as w:
+    json.dump(cal_dict,w, indent=4)
 
-cal_dict = {
-    "001": {
-        "operation1": {
-            "inputs":["cuspEmax"],
-            "outputs":["cal_energy"] ,
-            "function":"a+b*cuspEmax",
-            "pars":{"a":1, "b":1}
-        },
-
-        "operation2": {
-            "inputs":["A_max", "cuspEmax", "cuspEmax_ctc"],
-            "outputs":["aoe_classifier"] ,
-            "function":"(((A_max/cuspEmax)/(a+b*cuspEmax_ctc)) -1 )/sqrt(c+(d/cuspEmax_ctc)**e)",
-            "pars":{"a":1, "b":1, "c":1, "d":1, "e":1}
-        },
-
-        "operation3": {
-            "inputs":["aoe_classifier"],
-            "outputs":["aoe_pass"] ,
-            "function":"a<aoe_pass<b",
-            "pars":{"a":-1, "b":1}
-        }
-    },
-
-    "002": {
-        "operation1": {
-            "inputs":["cuspEmax"],
-            "outputs":["cal_energy"] ,
-            "function":"a+b*cuspEmax",
-            "pars":{"a":1, "b":1}
-        },
-
-        "operation2": {
-            "inputs":["A_max", "cuspEmax", "cuspEmax_ctc"],
-            "outputs":["aoe_classifier"] ,
-            "function":"(((A_max/cuspEmax)/(a+b*cuspEmax_ctc)) -1 )/sqrt(c+(d/cuspEmax_ctc)**e)",
-            "pars":{"a":1, "b":1, "c":1, "d":1, "e":1}
-        },
-
-        "operation3": {
-            "inputs":["aoe_classifier"],
-            "outputs":["aoe_pass"] ,
-            "function":"a<aoe_pass<b",
-            "pars":{"a":-1, "b":1}
-        }
-        
-    }
-
-}
-
-if args.hit_pars is not None:
-    pathlib.Path(os.path.dirname(args.hit_pars)).mkdir(parents=True, exist_ok=True)
-    with open(args.hit_pars, 'w') as w:
-        json.dump(out_dict,w, indent=4)
-pathlib.Path(args.plot_file).touch()
+pathlib.Path(os.path.dirname(args.aoe_results)).mkdir(parents=True, exist_ok=True)
+with open(args.aoe_results, 'w') as w:
+    json.dump(out_dict,w, indent=4)
--- scripts/pars_hit_ecal.py
+++ scripts/pars_hit_ecal.py
@@ -1,48 +1,54 @@
-import pygama.lh5 as lh5
 import numpy as np
 import os,json
 import pathlib
 import argparse
+import logging
 
 from pygama.pargen.ecal_th import energy_cal_th
-from pygama.pargen.ecal_other_sources import energy_cal_ba,energy_cal_co,energy_cal_am
 
 
+def run_energy_cal(files, lh5_path = 'dsp',plot_path=None, hit_dict={}):
 
-def run_energy_cal(files, det,measurement, cut_parameters ={"bl_mean":4, "bl_std":4, "pz_std":4},  plot_path=None):
-    
-    print(f'{len(files)} files found')
-
-    energy_params = ['trapEmax_ctc', 'cuspEmax_ctc','zacEmax_ctc','trapEftp_ctc','cuspEftp_ctc','zacEftp_ctc']
+    energy_params = ['cuspEmax_ctc', 'zacEmax_ctc', 'trapEmax_ctc'] #
         
-    if measurement == "th_HS2_top_psa" or measurement == "th_HS2_lat_psa":
-        out_dict = energy_cal_th(files, energy_params, plot_path = plot_path, cut_parameters= cut_parameters)
-    elif measurement == "ba_HS4_top_dlt":
-        out_dict = energy_cal_ba(files, energy_params, plot_path = plot_path, cut_parameters= cut_parameters)
-    elif measurement == "co_HS5_top_dlt":
-        out_dict = energy_cal_co(files, energy_params, plot_path = plot_path, cut_parameters= cut_parameters)
-    elif measurement == "am_HS1_lat_ssh" or measurement == "am_HS6_top_dlt":
-        out_dict = energy_cal_am(files, energy_params, plot_path = plot_path, cut_parameters= cut_parameters)
-    return out_dict
+    out_dict, result_dict = energy_cal_th(files, energy_params, lh5_path =lh5_path, hit_dict=hit_dict, 
+                                        plot_path = plot_path, p_val=0, threshold=100)  #,cut_parameters= cut_parameters
+
+    return out_dict, result_dict 
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser()
     argparser.add_argument("files", help="files", nargs='*',type=str)
+    argparser.add_argument("--ctc_dict", help="ctc_dict", type=str)
+    
     argparser.add_argument("--configs", help="config", type=str, required=True)
+    argparser.add_argument("--datatype", help="Datatype", type=str, required=True)
+    argparser.add_argument("--timestamp", help="Timestamp", type=str, required=True)
+    argparser.add_argument("--channel", help="Channel", type=str, required=True)
+    
+    argparser.add_argument("--log", help="log_file", type=str)
+    
     argparser.add_argument("--plot_path", help="plot_path", type=str)
     argparser.add_argument("--save_path", help="save_path", type=str)
+    argparser.add_argument("--results_path", help="results_path", type=str)
     args = argparser.parse_args()
 
-    
-    
-    with open(args.files[0]) as f:
-        files = f.read().splitlines()
-        files = sorted(files)[:10]
-    #out_dict = run_energy_cal(files, args.detector, args.measurement, cut_parameters = cut_parameters, plot_path = args.plot_path)
+    logging.basicConfig(level=logging.DEBUG, filename=args.log, filemode='w')
+    logging.getLogger('numba').setLevel(logging.INFO)
+    logging.getLogger('parse').setLevel(logging.INFO)
+    logging.getLogger('matplotlib.font_manager').setLevel(logging.INFO)
+    logging.getLogger('lgdo.lh5_store').setLevel(logging.INFO)
 
-    out_dict = {"ecal_pars":[1,1]}
+    with open(args.ctc_dict,'r') as r:
+        hit_dict = json.load(r)
+
+    out_dict, result_dict = run_energy_cal(args.files, lh5_path=f'{args.channel}/dsp', hit_dict=hit_dict, plot_path= args.plot_path) #cut_parameters = cut_parameters, 
+    
 
     with open(args.save_path,'w') as fp:
         pathlib.Path(os.path.dirname(args.save_path)).mkdir(parents=True, exist_ok=True)
         json.dump(out_dict,fp, indent=4)
-    pathlib.Path(args.plot_path).touch()
+
+    with open(args.results_path,'w') as fp:
+        pathlib.Path(os.path.dirname(args.results_path)).mkdir(parents=True, exist_ok=True)
+        json.dump(result_dict,fp, indent=4)
