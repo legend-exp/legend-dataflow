f720a61bee5ef7fb40894b5220cfa13cd38d121a
George Marshall <marshall@legend-login2.lngs.infn.it>
2022-08-15 18:02:30 +0200

    removed verbose argument

--- scripts/pars_dsp_tau.py
+++ scripts/pars_dsp_tau.py
@@ -34,9 +34,9 @@ if isinstance(input_file, list):
 
 pathlib.Path(os.path.dirname(args.output_file)).mkdir(parents=True, exist_ok=True)
 if args.plot_path:
-    out_dict = dpp.dsp_preprocess_decay_const(input_file, channel_dict, args.channel, plot_path=args.plot_path, verbose=True) 
+    out_dict = dpp.dsp_preprocess_decay_const(input_file, channel_dict, args.channel, plot_path=args.plot_path) 
 else:
-    out_dict = dpp.dsp_preprocess_decay_const(input_file, channel_dict, args.channel,verbose=True) 
+    out_dict = dpp.dsp_preprocess_decay_const(input_file, channel_dict, args.channel) 
 
 with open(args.output_file,"w") as f:
     json.dump(out_dict,f, indent=4)
