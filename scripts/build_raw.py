0b0e7ff89dbc8a3cea5a31bb48a31a5091f6104f
ggmarshall <george.marshall.20@ucl.ac.uk>
2022-11-30 17:13:36 +0100

    changed to import config

--- scripts/build_raw.py
+++ scripts/build_raw.py
@@ -1,13 +1,19 @@
-import argparse, os, pathlib
+import argparse, os, pathlib, json
 import logging
+
+from util.metadata_loading import *
+from util.Props import *
 
 import pygama
 from pygama.raw.build_raw import * 
 import numpy as np
 
+
 argparser = argparse.ArgumentParser()
 argparser.add_argument("input", help="input file", type=str)
 argparser.add_argument("output", help="output file", type=str)
+argparser.add_argument("--datatype", help="Datatype", type=str, required=True)
+argparser.add_argument("--timestamp", help="Timestamp", type=str, required=True)
 argparser.add_argument("--configs", help="config file", type=str)
 argparser.add_argument("--log", help="log file", type=str)
 args = argparser.parse_args()
@@ -18,34 +24,11 @@ pathlib.Path(os.path.dirname(args.output)).mkdir(parents=True, exist_ok=True)
 
 # ToDo: Atomic file creation
 
-out_spec = '''
-{
-  "ORFlashCamADCWaveformDecoder" : {
-    "ch{key:03d}/raw" : {
-      "key_list" : ["*"],
-      "out_stream" : "{filekey}"
-    }
-  },
-  "OrcaHeaderDecoder" : {
-    "OrcaHeader" : {
-      "key_list" : [ "*" ],
-      "out_stream" : "{filekey}"
-    }
-  },
-  "ORFlashCamListenerConfigDecoder" : {
-    "FCConfig" : {
-      "key_list" : [ "*" ],
-      "out_stream" : "{filekey}"
-    }
-  },
-  "*" : {
-    "{name}" : {
-      "key_list" : [ "*" ],
-      "out_stream" : "{filekey}"
-    }
-  }
-}
-'''
+cfg_file = os.path.join(args.configs, 'key_resolve.jsonl')
+channel_dict = config_catalog.get_config(cfg_file, args.configs, args.timestamp, args.datatype)
+channel_dict = channel_dict['snakemake_rules']['tier_raw']["inputs"]['raw_config']
+with open(channel_dict, "r") as f:
+  out_spec = json.load(f)
 
 rand_num = f'{np.random.randint(0,99999):05d}'
 temp_output = f'{args.output}.{rand_num}'
