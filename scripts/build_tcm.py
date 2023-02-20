d2d7b50e37f0b98c07e3289af6f3fb336283b2be
jasondet <jasondet@gmail.com>
2023-02-20 09:41:39 +0100

    make a hardware tcm for each fcid

--- scripts/build_tcm.py
+++ scripts/build_tcm.py
@@ -1,6 +1,8 @@
 import  os, logging
 import argparse, pathlib
 from pygama.evt.build_tcm import *
+from pygama import lgdo
+from pygama.raw.orca import orca_flashcam
 import numpy as np
 
 argparser = argparse.ArgumentParser()
@@ -17,6 +19,19 @@ pathlib.Path(os.path.dirname(args.output)).mkdir(parents=True, exist_ok=True)
 rand_num = f'{np.random.randint(0,99999):05d}'
 temp_output = f'{args.output}.{rand_num}'
 
-build_tcm([(args.input, '/ch*/raw')], 'eventnumber', out_file=temp_output, out_name='hardware_tcm', wo_mode='o')
+# get the list of channels by fcid
+ch_list = lgdo.ls(args.input, '/ch*')
+fcid_channels = {}
+for ch in ch_list:
+    key = int(ch[2:])
+    fcid = orca_flashcam.get_fcid(key)
+    if fcid not in fcid_channels: fcid_channels[fcid] = []
+    fcid_channels[fcid].append(f'/{ch}/raw')
 
-os.rename(temp_output, args.output)
+# make a hardware_tcm_[fcid] for each fcid
+for fcid in fcid_channels.keys():
+    out_name = f'hardware_tcm_{fcid}'
+    ch_list = fcid_channels[fcid]
+    build_tcm([(args.input, ch_list)], 'eventnumber', out_file=temp_output, out_name=out_name, wo_mode='o')
+
+#os.rename(temp_output, args.output)
