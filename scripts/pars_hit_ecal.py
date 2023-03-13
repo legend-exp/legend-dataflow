12cd8b9ee606861deb42b5f87c50e0c6a236db34
ggmarshall <george.marshall.20@ucl.ac.uk>
2023-03-13 13:26:34 +0100

    reformatted baseline plots and added common dict for plotting

--- scripts/pars_hit_ecal.py
+++ scripts/pars_hit_ecal.py
@@ -4,16 +4,98 @@ import pathlib
 import argparse
 import logging
 import pickle as pkl
+from datetime import datetime
+from scipy.stats import binned_statistic
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
 
 from legendmeta import LegendMetadata
 from legendmeta.catalog import Props
 
-from pygama.pargen.ecal_th import energy_cal_th
+import pygama.pargen.ecal_th as ect
 import pygama.pargen.cuts as cts
 import pygama.lgdo.lh5_store as lh5
 import pygama.math.histogram as pgh
 
 log = logging.getLogger(__name__)
+
+def plot_baseline_timemap(data, figsize = (12, 8), fontsize = 12 , parameter="bl_mean", dx=1,n_spread=5, time_dx=180):
+    plt.rcParams["figure.figsize"] = figsize
+    plt.rcParams["font.size"] = fontsize
+    
+    time_bins = np.arange(
+        (np.amin(data["timestamp"]) // time_dx) * time_dx,
+        ((np.amax(data["timestamp"]) // time_dx) + 2) * time_dx,
+        time_dx,
+    )
+    
+    mean = np.nanpercentile(data[parameter],50)
+    spread = mean-np.nanpercentile(data[parameter],10)
+    fig = plt.figure()
+    plt.hist2d(
+        data["timestamp"],
+        data[parameter],
+        bins=[time_bins, np.arange(mean-n_spread*spread, mean+n_spread*spread+dx, dx)],
+        norm=LogNorm(),
+    )
+
+    ticks, labels = plt.xticks()
+    plt.xlabel(
+        f"Time starting : {datetime.utcfromtimestamp(ticks[0]).strftime('%d/%m/%y %H:%M')}"
+    )
+    plt.ylabel("Baseline Value")
+    plt.ylim([mean-n_spread*spread, mean+n_spread*spread])
+
+    plt.xticks(
+        ticks,
+        [datetime.utcfromtimestamp(tick).strftime("%H:%M") for tick in ticks],
+    )
+    plt.close()
+    return fig
+
+def bin_bl_stability(data, 
+                   time_slice= 180, parameter="bl_mean", dx=1):
+    
+    utime_array = data["timestamp"]
+    select_bls = data[parameter].to_numpy()
+    
+    time_bins = np.arange(
+        (np.amin(utime_array) // time_slice) * time_slice,
+        ((np.amax(utime_array) // time_slice) + 2) * time_slice,
+        time_slice,
+    )
+    # bin time values
+    times_average = (time_bins[:-1] + time_bins[1:]) / 2
+
+    nanmedian = lambda x : np.nanpercentile(x,50) if len(x)>=10 else np.nan
+    error = lambda x : np.nanvar(x)/np.sqrt(len(x)) if len(x)>=10 else np.nan
+
+    par_average,_,_ = binned_statistic(data["timestamp"], select_bls, statistic=nanmedian,
+                                        bins=time_bins)
+    par_error,_,_ = binned_statistic(data["timestamp"], select_bls, statistic=error,
+                                        bins=time_bins)
+        
+    return {"time": times_average, "baseline":par_average, "spread": par_error}
+
+def bin_baseline(data, parameter="bl_mean-baseline", dx=1, bl_range=[-500,500]):
+    par_array = data.eval(parameter)
+    bins = np.arange(bl_range[0],bl_range[1],dx)
+    bl_array, bins,_ = pgh.get_hist(par_array, bins=bins)
+    return {"bl_array":bl_array, "bins":(bins[1:]+bins[:-1])/2}
+
+
+
+def baseline_tracking_plots(files, lh5_path, plot_options={}):
+    plot_dict={}
+    data = lh5.load_dfs(files,["bl_mean", "baseline", "timestamp"], lh5_path)
+    for key, item in plot_options.items():
+        if item["options"] is not None:
+            plot_dict[key]=item["function"](data, **item["options"])
+        else:
+            plot_dict[key]=item["function"](data)
+    return plot_dict
+
+
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser()
@@ -52,19 +134,43 @@ if __name__ == '__main__':
     with open(channel_dict,"r") as r:
         kwarg_dict = json.load(r)
 
-    if args.plot_path:
-        out_dict, result_dict,plot_dict = energy_cal_th(sorted(args.files), lh5_path=f'{args.channel}/dsp', hit_dict=hit_dict, 
-                                            display=1, **kwarg_dict)  
+    plot_dict = kwarg_dict["plot_options"]
+    for field,item in plot_dict.items():
+        plot_dict[field]["function"]=eval(item["function"])
 
-        bins = np.arange(-500,500,1)
-        bls = lh5.load_nda(sorted(args.files),["bl_mean"], f'{args.channel}/dsp')["bl_mean"]
-        bl_array, bins,_ = pgh.get_hist(bls, bins=bins)
-        plot_dict["baseline"] = {"bl_array":bl_array, "bins":(bins[1:]+bins[:-1])/2}
+    kwarg_dict["plot_options"]=plot_dict 
+    bl_plots = kwarg_dict.pop("bl_plot_options")
+    for field,item in bl_plots.items():
+        bl_plots[field]["function"]=eval(item["function"])
+    common_plots = kwarg_dict.pop("common_plots")
+
+    if args.plot_path:
+        out_dict, result_dict,plot_dict = ect.energy_cal_th(sorted(args.files), lh5_path=f'{args.channel}/dsp', 
+                                        hit_dict=hit_dict, **kwarg_dict)  
+
+        common_dict = baseline_tracking_plots(sorted(args.files), f'{args.channel}/dsp', plot_options=bl_plots)
+
+        for plot in list(common_dict):
+            if plot not in common_plots:
+                plot_item = common_dict.pop(plot)
+                plot_dict.update({plot: plot_item}) 
+        
         pathlib.Path(os.path.dirname(args.plot_path)).mkdir(parents=True, exist_ok=True)
+
+        for key, item in plot_dict.items():
+            if isinstance(item,dict):
+                if len(item)>0:
+                    param_dict = {}
+                    for plot in common_plots:
+                        if plot in item:
+                            param_dict.update({plot:item[plot]})
+                    common_dict.update({key:param_dict})
+        plot_dict["common"] = common_dict
+
         with open(args.plot_path,"wb") as f:
             pkl.dump(plot_dict,f, protocol= pkl.HIGHEST_PROTOCOL)
     else:
-        out_dict, result_dict = energy_cal_th(sorted(args.files), lh5_path=f'{args.channel}/dsp', hit_dict=hit_dict, 
+        out_dict, result_dict,_ = ect.energy_cal_th(sorted(args.files), lh5_path=f'{args.channel}/dsp', hit_dict=hit_dict, 
                                             **kwarg_dict)
     
     out_dict.update({"cuspEmax_cal": {
