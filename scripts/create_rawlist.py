a16ca801a6257cbc8705b7d3e3842f4d0d84f1a4
ggmarshall <george.marshall.20@ucl.ac.uk>
2023-03-14 15:29:00 +0100

    split rawlist into own file as in future will work from raw up

--- scripts/create_rawlist.py
+++ scripts/create_rawlist.py
@@ -1,14 +1,24 @@
 from util.FileKey import *
 import glob
+import os
+
+configs=snakemake.input[0]
+ignored_keyslist = os.path.join(configs, 'ignore_keys.keylist')
+if os.path.isfile(ignored_keyslist):
+    with open(ignored_keyslist, 'r') as f:
+        ignore_keys = f.read().splitlines()
+else:
+    print("no ignore_keys.keylist file found")
 
 keypart = snakemake.wildcards.keypart
 setup = snakemake.params.setup
+
 key = FileKey.parse_keypart(keypart)
 
 item_list = []
 for item in key:
-    if "&" in item:
-        item = item.split("&")
+    if "_" in item:
+        item = item.split("_")
     if isinstance(item, list):
         item_list.append(item)
     else:
@@ -24,11 +34,15 @@ for i in item_list[0]:
 
 keys = []
 for key in filekeys:
-    fn_glob_pattern = key.get_path_from_filekey(get_pattern_unsorted_data(setup))[0]
+    fn_glob_pattern = key.get_path_from_filekey(get_pattern_tier_daq(setup))[0]
     files = glob.glob(fn_glob_pattern)
+
     for f in files:
-        key = FileKey.get_filekey_from_pattern(f,get_pattern_unsorted_data(setup))
-        keys.append(key.name)
+        key = FileKey.get_filekey_from_pattern(f,get_pattern_tier_daq(setup))
+        if key.name in ignore_keys:
+            pass
+        else:
+            keys.append(key.name)
 
 with open(snakemake.output[0], 'w') as f:
     for key in keys:
