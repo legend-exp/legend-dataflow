d2d8952ad12f01ef09110cf2f21d477e829328a6
ggmarshall <george.marshall.20@ucl.ac.uk>
2023-08-01 18:14:28 +0200

    updated for new pygama aoe

--- scripts/pars_hit_aoe.py
+++ scripts/pars_hit_aoe.py
@@ -7,7 +7,7 @@ import pickle as pkl
 
 from legendmeta import LegendMetadata
 
-from pygama.pargen.AoE_cal import cal_aoe
+from pygama.pargen.AoE_cal import *
 
 
 argparser = argparse.ArgumentParser()
@@ -32,33 +32,43 @@ logging.basicConfig(level=logging.DEBUG, filename=args.log, filemode='w')
 logging.getLogger('numba').setLevel(logging.INFO)
 logging.getLogger('parse').setLevel(logging.INFO)
 
-with open(args.files[0]) as f:
-    files = f.read().splitlines()
-files = sorted(files)
+configs = LegendMetadata(path = args.configs)
+channel_dict = configs.on(args.timestamp, system=args.datatype)['snakemake_rules']['pars_hit_aoecal']["inputs"]['aoecal_config'][args.channel]
+
+with open(channel_dict, "r") as r:
+    kwarg_dict = json.load(r)
 
 with open(args.ecal_file, 'r') as o:
     cal_dict = json.load(o)
 
-configs = LegendMetadata(path = args.configs)
-channel_dict = configs.on(args.timestamp, system=args.datatype)['snakemake_rules']['pars_hit_aoecal']["inputs"]['aoecal_config'][args.channel]
-
-with open(channel_dict,"r") as r:
-    kwarg_dict = json.load(r)
-
 with open(args.eres_file, 'r') as o:
     eres_dict = json.load(o)
 
-eres_pars = eres_dict[kwarg_dict["cal_energy_param"]]['eres_pars']
-
 if kwarg_dict["run_aoe"] ==True:
     kwarg_dict.pop("run_aoe")
+
+    if "pdf" in kwarg_dict:
+        pdf =eval(kwarg_dict.pop("pdf"))
+    else:
+        pdf = standard_aoe
+    
+    if "sigma_func" in kwarg_dict:
+        sigma_func =eval(kwarg_dict.pop("sigma_func"))
+    else:
+        sigma_func = sigma_fit
+
+    with open(args.files[0]) as f:
+        files = f.read().splitlines()
+    files = sorted(files)
+
+    eres_pars = eres_dict[kwarg_dict["cal_energy_param"]]['eres_pars']
     
     if args.plot_file:
         cal_dict, out_dict,plot_dict = cal_aoe(files, lh5_path=f'{args.channel}/dsp', cal_dict=cal_dict, 
-                                eres_pars=eres_pars, display=1, **kwarg_dict) 
+                                eres_pars=eres_pars, display=1, pdf=pdf, sigma_func=sigma_func, **kwarg_dict) 
     else:
         cal_dict, out_dict,plot_dict = cal_aoe(files, lh5_path=f'{args.channel}/dsp', cal_dict=cal_dict, 
-                                eres_pars=eres_pars, **kwarg_dict) 
+                                eres_pars=eres_pars, pdf=pdf, sigma_func=sigma_func,  **kwarg_dict) 
 else:
     out_dict = {}
     plot_dict = {}
@@ -84,11 +94,9 @@ if args.plot_file:
 
 final_hit_dict = { "operations":cal_dict}
 
-
-
 pathlib.Path(os.path.dirname(args.hit_pars)).mkdir(parents=True, exist_ok=True)
 with open(args.hit_pars, 'w') as w:
-    json.dump(final_hit_dict,w, indent=4)
+    json.dump(final_hit_dict, w, indent=4)
 
 pathlib.Path(os.path.dirname(args.aoe_results)).mkdir(parents=True, exist_ok=True)
 with open(args.aoe_results, 'w') as w:
