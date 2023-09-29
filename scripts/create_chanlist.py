8f71dd314dae7e3e71a65df872a98219ef096995
ggmarshall <george.marshall.20@ucl.ac.uk>
2023-09-29 18:06:12 +0200

    now can specify just to run on analysis runs

--- scripts/create_chanlist.py
+++ scripts/create_chanlist.py
@@ -16,11 +16,9 @@ if "_" in tier:
     name = tier.split("_", 1)[1]
     tier = tier.split("_", 1)[0]
 
-print(keypart)
 key = ChannelProcKey.parse_keypart(keypart)
 
 output_file = os.path.join(filelist_path(setup),f"all-{key.experiment}-{key.period}-{key.run}-cal-{key.timestamp}-channels.chankeylist.{random.randint(0,99999):05d}")
-print(output_file)
 
 cmd = f"{runcmd(setup)} python3 -B {basedir}/scripts/create_chankeylist.py --configs {configs} --channelmap {chan_maps} --timestamp {key.timestamp} --datatype cal --output_file {output_file}"
 os.system(cmd)
--- scripts/create_filelist.py
+++ scripts/create_filelist.py
@@ -1,16 +1,17 @@
 import snakemake as smk
-import os, re, glob
+import os, re, glob, json
 
 from util.FileKey import *
 
 setup = snakemake.params.setup
 
+file_selection = snakemake.wildcards.label[:3]
 tier = snakemake.wildcards.tier
-keypart=snakemake.wildcards.label.replace("all","")
+keypart=snakemake.wildcards.label[3:]#.replace("all","")
 search_pattern = snakemake.params.search_pattern
 
 ignore_keys = []
-if snakemake.input:
+if snakemake.params.configs:
     configs=snakemake.params.configs
     ignored_keyslist = os.path.join(configs, 'ignore_keys.keylist')
     if os.path.isfile(ignored_keyslist):
@@ -18,6 +19,13 @@ if snakemake.input:
             ignore_keys = f.read().splitlines()
     else:
         print("no ignore_keys.keylist file found")
+
+    analysis_runs_file = os.path.join(configs, 'analysis_runs.json')
+    if os.path.isfile(analysis_runs_file):
+        with open(analysis_runs_file, 'r') as f:
+            analysis_runs = json.load(f)
+    else:
+        print("no analysis_runs file found")
 
 key = FileKey.parse_keypart(keypart)
 
@@ -42,13 +50,23 @@ keys = []
 for key in filekeys:
     fn_glob_pattern = key.get_path_from_filekey(search_pattern)[0]
     files = glob.glob(fn_glob_pattern)
-
     for f in files:
         key = FileKey.get_filekey_from_pattern(f,search_pattern)
         if key.name in ignore_keys:
             pass
         else:
-            keys.append(key.name)
+            if file_selection == 'all':
+                keys.append(key.name)
+            elif file_selection == "sel":
+                if analysis_runs == "all":
+                    keys.append(key.name)
+                else:
+                    if key.period in analysis_runs:
+                        if key.run in analysis_runs[key.period] or analysis_runs[key.period]=="all":
+                            keys.append(key.name)
+            else:
+                raise ValueError("unknown file selection")
+keys = sorted(keys)
 
 
 filenames = FileKey.tier_files(setup, keys, tier)
