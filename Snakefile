legend-exp/legend-dataflow

commit ae67c3ce12747d5715e568a07c8dbdf74fad5560

Fix linting issues with function arguments

scripts/util/create_pars_keylist.py
@@ -3,6 +3,7 @@ import json
 import os
 import pathlib
 import re
+from typing import ClassVar
 
 import snakemake as smk
 
@@ -12,7 +13,7 @@ from .utils import *
 
 
 class pars_key_resolve:
-    name_dict = {"cal": ["par_dsp", "par_hit"], "lar": ["par_dsp", "par_hit"]}
+    name_dict: ClassVar[dict] = {"cal": ["par_dsp", "par_hit"], "lar": ["par_dsp", "par_hit"]}
 
     def __init__(self, valid_from, category, apply):
         self.valid_from = valid_from
@@ -89,7 +90,7 @@ class pars_key_resolve:
         return out_list
 
     @staticmethod
-    def get_keys(keypart, setup, search_pattern):
+    def get_keys(keypart, search_pattern):
         d = FileKey.parse_keypart(keypart)
         tier_pattern_rx = re.compile(smk.io.regex(search_pattern))
         fn_glob_pattern = smk.io.expand(search_pattern, **d._asdict())[0]

scripts/pars_hit_aoe.py
@@ -78,7 +78,7 @@ if kwarg_dict["run_aoe"] is True:
 
     except:
 
-        def eres_func(x):
+        def eres_func():
             return np.nan
 
     cal_dict, out_dict, plot_dict, obj = aoe_calibration(

scripts/pars_hit_ecal.py
@@ -63,7 +63,7 @@ def plot_baseline_timemap(
     return fig
 
 
-def bin_bl_stability(data, time_slice=180, parameter="bl_mean", dx=1):
+def bin_bl_stability(data, time_slice=180, parameter="bl_mean"):
     utime_array = data["timestamp"]
     select_bls = data[parameter].to_numpy()
 

scripts/pars_pht.py
@@ -180,7 +180,7 @@ if aoe_options.pop("run_aoe") is True:
 
         except:
 
-            def eres_func(x):
+            def eres_func():
                 return np.nan
 
     cal_dict, out_dict, plot_dict, aoe_obj = aoe_calibration(

scripts/util/dataset_cal.py
@@ -95,7 +95,6 @@ class dataset_file:
         experiment="l200",
         datatype="cal",
         name=None,
-        extension="json",
     ):
         dataset = self.get_dataset(dataset, channel)
         all_par_files = []

commit 5a6d5683c4aca48fbafe8f9d7fd435f6e6c6d54f

Fix bug introduced in ae67c3c

Snakefile
@@ -25,7 +25,6 @@ localrules:
 
 
 ds.pars_key_resolve.write_par_catalog(
-    setup,
     ["-*-*-*-cal"],
     os.path.join(pars_path(setup), "pht", "validity.jsonl"),
     get_pattern_tier_raw(setup),

scripts/util/create_pars_keylist.py
@@ -105,12 +105,12 @@ class pars_key_resolve:
         return keys
 
     @staticmethod
-    def write_par_catalog(setup, keypart, filename, search_pattern, name_dict):
+    def write_par_catalog(keypart, filename, search_pattern, name_dict):
         if isinstance(keypart, str):
             keypart = [keypart]
         keylist = []
         for keypar in keypart:
-            keylist += pars_key_resolve.get_keys(keypar, setup, search_pattern)
+            keylist += pars_key_resolve.get_keys(keypar, search_pattern)
         keys = sorted(keylist, key=FileKey.get_unix_timestamp)
         keylist = pars_key_resolve.generate_par_keylist(keys)
 