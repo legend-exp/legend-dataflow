590ceedd2abb3c52ee52f70ee48f79f36da24a93
ggmarshall <george.marshall.20@ucl.ac.uk>
2023-10-26 18:00:34 +0200

    chanegd script to run with snakemake

--- scripts/blinding_calibration.py
+++ scripts/blinding_calibration.py
@@ -5,90 +5,96 @@ and deriving a simple scaling relation from adc to keV.
 """
 
 import argparse
-import glob
 import json
 import logging
-import os
+import pickle as pkl
 
 import lgdo.lh5_store as lh5
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 from legendmeta import LegendMetadata
-from matplotlib.backends.backend_pdf import PdfPages
 from pygama.math.histogram import better_int_binning, get_hist
 from pygama.pargen.energy_cal import hpge_find_E_peaks
 
+mpl.use("agg")
+
 argparser = argparse.ArgumentParser()
-argparser.add_argument("input", help="input file path", type=str)
-argparser.add_argument("output", help="output file", type=str)
-argparser.add_argument("meta", help="meta path", type=str)
-argparser.add_argument("out_plot", help="out plot path", type=str)
+argparser.add_argument("--files", help="files", nargs="*", type=str)
+argparser.add_argument("--blind_curve", help="blind_curve", type=str)
+argparser.add_argument("--plot_file", help="out plot path", type=str)
+argparser.add_argument("--meta", help="meta", type=str)
+argparser.add_argument("--timestamp", help="timestamp", type=str)
+argparser.add_argument("--datatype", help="datatype", type=str)
+argparser.add_argument("--channel", help="channel", type=str)
+argparser.add_argument("--configs", help="configs", type=str)
+argparser.add_argument("--log", help="log", type=str)
 args = argparser.parse_args()
 
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG, filename=args.log, filemode="w")
 logging.getLogger("numba").setLevel(logging.INFO)
 logging.getLogger("parse").setLevel(logging.INFO)
 logging.getLogger("lgdo").setLevel(logging.INFO)
 logging.getLogger("matplotlib").setLevel(logging.INFO)
+log = logging.getLogger(__name__)
 
-# get all files in specified directory
-files = sorted(glob.glob(os.path.join(args.input, "*.lh5")))
 
 # load in channel map
 meta = LegendMetadata(args.meta)
-chmap = meta.channelmap(os.path.basename(files[0]).split("-")[4])
-
-# get list of all processable detectors
-working_dets = [
-    det
-    for det, dic in chmap.map("system", unique=False)["geds"].map("name").items()
-    if dic["analysis"]["processable"] is True
-]
+chmap = meta.channelmap(args.timestamp)
 
 pars_dict = {}
 # peaks to search for
 peaks_keV = np.array([238, 583.191, 727.330, 860.564, 1592.53, 1620.50, 2103.53, 2614.50])
 
-# get calobration for each channel
-with PdfPages(args.out_plot) as pdf:
-    for det in working_dets:
-        E_uncal = lh5.load_nda(files, ["daqenergy"], f"ch{chmap[det].daq.rawid}/raw")["daqenergy"]
 
-        guess_keV = 2620 / np.nanpercentile(E_uncal, 99)  # usual simple guess
-        Euc_min = peaks_keV[0] / guess_keV * 0.6
-        Euc_max = peaks_keV[-1] / guess_keV * 1.1
-        dEuc = 1 / guess_keV
+E_uncal = lh5.load_nda(sorted(args.files), ["daqenergy"], f"{args.channel}/raw")["daqenergy"]
 
-        # daqenergy is an int so use integer binning (dx used to be bugged as output so switched to nbins)
-        Euc_min, Euc_max, nbins = better_int_binning(
-            x_lo=Euc_min, x_hi=Euc_max, n_bins=(Euc_max - Euc_min) / dEuc
-        )
-        hist, bins, var = get_hist(E_uncal, range=(Euc_min, Euc_max), bins=nbins)
+guess_keV = 2620 / np.nanpercentile(E_uncal, 99)  # usual simple guess
+Euc_min = peaks_keV[0] / guess_keV * 0.6
+Euc_max = peaks_keV[-1] / guess_keV * 1.1
+dEuc = 1 / guess_keV
 
-        # Run the rough peak search
-        detected_peaks_locs, detected_peaks_keV, roughpars = hpge_find_E_peaks(
-            hist, bins, var, peaks_keV, n_sigma=5, deg=0
-        )
-        # dictionary to pass to build hit
-        out_dict = {
-            "daqenergy_cal": {
-                "expression": "daqenergy*a",
-                "parameters": {"a": round(roughpars[0], 5)},
-            }
-        }
-        pars_dict[f"ch{chmap[det].daq.rawid}"] = out_dict
+# daqenergy is an int so use integer binning (dx used to be bugged as output so switched to nbins)
+Euc_min, Euc_max, nbins = better_int_binning(
+    x_lo=Euc_min, x_hi=Euc_max, n_bins=(Euc_max - Euc_min) / dEuc
+)
+hist, bins, var = get_hist(E_uncal, range=(Euc_min, Euc_max), bins=nbins)
 
-        # plot to check thagt the calibration is correct with zoom on 2.6 peak
-        fig = plt.figure()
-        plt.hist(E_uncal * roughpars[0], bins=np.arange(0, 3000, 1), histtype="step")
-        plt.xlabel("energy (keV)")
-        plt.ylabel("counts")
-        plt.yscale("log")
-        plt.title(det)
-        pdf.savefig()
-        plt.close()
+# Run the rough peak search
+detected_peaks_locs, detected_peaks_keV, roughpars = hpge_find_E_peaks(
+    hist, bins, var, peaks_keV, n_sigma=5, deg=0
+)
+
+log.info(f"{len(detected_peaks_locs)} peaks found:")
+log.info("\t   Energy   | Position  ")
+for i, (Li, Ei) in enumerate(zip(detected_peaks_locs, detected_peaks_keV)):
+    log.info(f"\t{i}".ljust(4) + str(Ei).ljust(9) + f"| {Li:g}".ljust(5))  # noqa: G003
+
+# dictionary to pass to build hit
+out_dict = {
+    "daqenergy_cal": {
+        "expression": "daqenergy*a",
+        "parameters": {"a": round(roughpars[0], 5)},
+    }
+}
+
+# plot to check thagt the calibration is correct with zoom on 2.6 peak
+fig = plt.figure(figsize=(8, 10))
+ax = plt.subplot(211)
+ax.hist(E_uncal * roughpars[0], bins=np.arange(0, 3000, 1), histtype="step")
+ax.set_ylabel("counts")
+ax.set_yscale("log")
+ax2 = plt.subplot(212)
+ax2.hist(E_uncal * roughpars[0], bins=np.arange(2600, 2630, 1 * roughpars[0]), histtype="step")
+ax2.set_xlabel("energy (keV)")
+ax2.set_ylabel("counts")
+plt.suptitle(args.channel)
+with open(args.plot_file, "wb") as w:
+    pkl.dump(fig, w, protocol=pkl.HIGHEST_PROTOCOL)
+plt.close()
 
 
-with open(args.output, "w") as w:
-    json.dump(pars_dict, w, indent=4)
+with open(args.blind_curve, "w") as w:
+    json.dump(out_dict, w, indent=4)
