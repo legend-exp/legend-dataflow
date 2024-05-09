c48ddf5f48f6bcc4db59116bc6598c031724cc58
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2024-05-09 17:04:13 +0200

    Fix build_skm.py and forward LGDO attributes
    Still untested

--- scripts/build_skm.py
+++ scripts/build_skm.py
@@ -40,22 +40,45 @@ out_fields = Props.read_from(skm_config_file)["keep_fields"]
 store = lh5.LH5Store(keep_open=True)
 
 # read concatenated evt file in chunks to keep memory usage low
+# TODO: optimize buffer_len
 for chunk, entry, n_rows in lh5.LH5Iterator(args.evt_file, "evt", buffer_len=32768):
     evt = chunk.view_as("ak")
 
     # remove unwanted events
     skm = eval(f"evt[{evt_filter}]")
-    rect_skm = ak.flatten(skm, axis=-1)
+    # make it rectangular and make an LGDO Table
+    out_table = Table(ak.flatten(skm, axis=1))
 
     # remove unwanted columns
-    table = Table(rect_skm)
-    for field in table:
-        if field not in out_fields:
-            ptr = table
-            items = field.split(".")
+    out_table_skm = Table(size=len(out_table))
+    for field in out_fields:
+        # table nesting is labeled by '.' in the config
+        items = field.split(".")
+
+        # add column to output table
+        if field in out_fields:
+            # get to actual nested field recursively
+            ptr1 = out_table
+            ptr2 = out_table_skm
             for item in items[:-1]:
-                ptr = ptr[item]
-            ptr.remove_column(items[-1])
+                # make intermediate tables in new table
+                if item not in ptr2:
+                    ptr2.add_field(item, Table(size=(out_table)))
+
+                # get non-table LGDO recursively
+                ptr1 = ptr1[item]
+                ptr2 = ptr2[item]
+
+            # finally add column to new table
+            ptr2.add_field(items[-1], ptr1[item])
+
+            # forward LGDO attributes
+            attrs = evt.flatten()[field.replace(".", "_")].attrs
+            for attr, val in attrs:
+                if attr != "datatype":
+                    ptr2.attrs[attr] = val
 
     # write-append to disk
-    store.write(table, "skm", args.output, n_rows=n_rows, wo_mode=("w" if entry == 0 else "a"))
+    store.write(
+        out_table_skm, "skm", args.output, n_rows=n_rows, wo_mode=("w" if entry == 0 else "a")
+    )
