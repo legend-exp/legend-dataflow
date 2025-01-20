0b558ddb43988f1134e58d9dce61c9c8b1b295ea
ggmarshall <george.marshall.20@ucl.ac.uk>
2025-01-20 21:22:28 +0100

    fix weird filelist len bug by moving to script

---
+++ scripts/write_filelist.py
@@ -0,0 +1,14 @@
+# ruff: noqa: F821, T201
+# from snakemake.script import snakemake # snakemake > 8.16
+from pathlib import Path
+
+print(f"INFO: found {len(snakemake.input)} files")
+if len(snakemake.input) == 0:
+    print(
+        f"WARNING: no DAQ files found for the given pattern: {snakemake.wildcards.label}. "
+        "make sure patterns follows the format: "
+        "all-{experiment}-{period}-{run}-{datatype}-{timestamp}-{tier}.gen"
+    )
+with Path(snakemake.output[0]).open("w") as f:
+    for fn in snakemake.input:
+        f.write(f"{fn}\n")
