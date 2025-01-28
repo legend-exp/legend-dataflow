0c4b270de18d1c478134e54574ffee4737596de5
ggmarshall <george.marshall.20@ucl.ac.uk>
2025-01-28 18:42:48 +0100

    add execenv scripts

--- workflow/scripts/library/__init__.py
+++ workflow/scripts/library/__init__.py
@@ -1,5 +1,4 @@
 from .cal_grouping import CalGrouping
-from .catalog import Catalog, Props, PropsStream
 from .create_pars_keylist import ParsKeyResolve
 from .FileKey import ChannelProcKey, FileKey, ProcessingFileKey
 from .pars_loading import ParsCatalog
@@ -13,14 +12,11 @@ from .utils import (
 
 __all__ = [
     "CalGrouping",
-    "Catalog",
     "ChannelProcKey",
     "FileKey",
     "ParsCatalog",
     "ParsKeyResolve",
     "ProcessingFileKey",
-    "Props",
-    "PropsStream",
     "runcmd",
     "subst_vars",
     "subst_vars",
