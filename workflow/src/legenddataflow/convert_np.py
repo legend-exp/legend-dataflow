8a88d5fb08c9c47ea27259c4c317be0eaea0610e
ggmarshall <george.marshall.20@ucl.ac.uk>
2025-02-09 18:21:36 +0100

    docstrings and typehints

--- workflow/src/legenddataflow/convert_np.py
+++ workflow/src/legenddataflow/convert_np.py
@@ -1,15 +1,28 @@
 import numpy as np
 
 
-def convert_dict_np_to_float(dic):
-    for key in dic:
-        if isinstance(dic[key], dict):
-            convert_dict_np_to_float(dic[key])
-        elif isinstance(dic[key], (np.float32, np.float64)):
-            dic[key] = float(dic[key])
+def convert_dict_np_to_float(dic: dict) -> dict:
+    """
+    Convert all numpy floats in a dictionary to Python floats.
+
+    Parameters
+    ----------
+    dic : dict
+        The dictionary to convert.
+
+    Returns
+    -------
+    dict
+        The dictionary with all numpy floats converted to Python floats.
+    """
+    for key, value in dic.items():
+        if isinstance(value, dict):
+            convert_dict_np_to_float(value)
+        elif isinstance(value, (np.float32, np.float64)):
+            dic[key] = float(value)
         elif isinstance(dic[key], (list, tuple)):
             dic[key] = [
                 float(x) if isinstance(x, (np.float32, np.float64)) else x
-                for x in dic[key]
+                for x in value
             ]
     return dic
--- workflow/src/legenddataflow/log.py
+++ workflow/src/legenddataflow/log.py
@@ -5,7 +5,25 @@ from pathlib import Path
 from dbetto import Props
 
 
-def build_log(config_dict, log_file=None):
+def build_log(config_dict: dict, log_file: str | None = None) -> logging.Logger:
+    """
+    Build a logger from a configuration dictionary.
+    If a log file is provided, the logger will write to that file.
+
+    Parameters
+    ----------
+
+    config_dict : dict
+        A dictionary containing the logging configuration.
+    log_file : str, optional
+        The path to the log file.
+
+    Returns
+    -------
+    Logger
+        The logger object.
+
+    """
     if "logging" in config_dict["options"]:
         log_config = config_dict["options"]["logging"]
         log_config = Props.read_from(log_config)
