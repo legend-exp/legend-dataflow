05426afb63131dc991e740c1fdb054018559b5ca
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2025-02-12 19:10:59 +0100

    add function to get dataflow-config values

--- tests/runprod/conftest.sh
+++ tests/runprod/conftest.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
-PRODENV="$(realpath ..)"
-export PRODENV
+# IMPORTANT: this script must be *sourced* from the legend-dataflow directory
 
-echo "INFO: setting up test environment"
+PRODCYCLE="$(realpath .)"
+PRODENV="$(realpath ..)"
+
+function get_dataflow_config_value() {
+    python -c "import dbetto; print(dbetto.AttrsDict(dbetto.utils.load_dict('${PRODCYCLE}/dataflow-config.yaml')).${1})" \
+        | sed "s|\$_|${PRODCYCLE}|g"
+}
+
+export PRODENV PRODCYCLE
+
+echo "DEBUG: setting up test environment"
 
 python -m pip --quiet install --upgrade pip wheel setuptools
 python -m pip --quiet install --upgrade '.[runprod]'
--- tests/runprod/test-build-raw.sh
+++ tests/runprod/test-build-raw.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+# IMPORTANT: this script must be executed from the legend-dataflow directory
+
 # shellcheck disable=SC1091
+
 source "$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)/conftest.sh"
 
 snakemake --workflow-profile workflow/profiles/lngs-build-raw -n all-*-daq.gen
