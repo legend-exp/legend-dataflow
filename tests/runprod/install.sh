a5ce23eb3a6a11bb8f74e29fde9c57626254da7f
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2025-02-20 15:52:46 +0100

    [ci] use GHA workflow commants for logging
    https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/workflow-commands-for-github-actions

--- tests/runprod/install.sh
+++ tests/runprod/install.sh
@@ -2,7 +2,7 @@
 
 # IMPORTANT: this script must be executed from the legend-dataflow directory
 
-printf "\033[32m%s\033[0m\n" "DEBUG: setting up test environment"
+echo "::group::setting up test environment"
 
 PRODENV="$(realpath ..)"
 export PRODENV
@@ -11,3 +11,5 @@ python -m pip --quiet install --upgrade pip wheel setuptools
 python -m pip --quiet install --upgrade '.[runprod]'
 
 dataprod -v install --remove --system bare -- dataflow-config.yaml
+
+echo "::endgroup::"
--- tests/runprod/run-all.sh
+++ tests/runprod/run-all.sh
@@ -5,6 +5,7 @@
 ./tests/runprod/install.sh
 
 for test in tests/runprod/test-*.sh; do
-    printf "\033[32m%s\033[0m\n" "INFO: running test $test"
+    echo "::group::test $test"
     ./"$test"
+    echo "::endgroup::"
 done
